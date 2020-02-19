91747aa8438e553d3e018a44918cd2b56b77ed21
Nina Herasymova <predvoditeldvoryanstva@gmail.com>
2020-02-19 09:16:05 +0200

    add logic calc

--- src/calculator/script.js
+++ src/calculator/script.js
@@ -1,51 +1,118 @@
-let buttons = document.querySelectorAll('.flex div');
+let buttons = document.querySelectorAll('.flex input');
 let number = document.querySelectorAll('.number');
 let sign = document.querySelectorAll('.sign');
 let result = document.querySelector('.result');
 let clear = document.querySelector('.clear');
 let inp = document.querySelector('#inp');
-let resOutput = document.querySelector('#resOutput');
 let preview = document.querySelector('#preview');
-let operand1;
-let operand2;
-let operator;
+let dot = document.querySelector('#dot');
+let val1 = '';
+let val2 = '';
+let oper = '';
+let isOpClick = false;
+let isDot=false;
+let num;
 
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = function () {
-        if (buttons[i].classList == 'number') {
-            inp.value += this.innerHTML
-            preview.textContent += this.innerHTML;
+for (let i = 0; i < number.length; i++) {
+    number[i].addEventListener('click', getValue);
+    function getValue() {
+        num = this.value;
+        if (isOpClick == false) {
+            val1 += num;
+            inp.value = val1;
+            preview.textContent += num;
+            if(inp.value.indexOf('.') !== -1){
+                isDot=true;
+            }
+            return val1;           
         }
-        else if (buttons[i].classList == 'sign') {
-            operand1 = inp.value;
-            inp.value += this.innerHTML;
-            preview.textContent += this.innerHTML;
-        }
-        if (inp.value == '+' || inp.value == '-' || inp.value == '*' || inp.value == '/') {
-            operator = inp.value;
-            inp.value += this.innerHTML;
-            preview.textContent += this.innerHTML;
-        }        
-        if (buttons[i].classList == 'result') {
-            operand2 = inp.value;
-            inp.value = getResult()
-            preview.textContent += ('=' + getResult());
+        if(isOpClick == true){
+        val2 += num;
+        inp.value = val2;
+        preview.textContent += num;
+        return val2;
         }
     }
 }
+
+dot.addEventListener('click', addDot);
+function addDot() {
+
+    if (isDot === false && isOpClick === false) {
+        val1 += this.value;
+        inp.value += this.value;
+        preview.textContent += this.value;
+        isDot = true;
+    } else if (isDot === true && isOpClick === true) {
+        val2 += this.value;
+        inp.value += this.value;
+        preview.textContent += this.value;
+       isDot = false;
+    }return;
+} 
+        
+    
+
+for (let i = 0; i < sign.length; i++) {
+    sign[i].addEventListener('click', getValue);
+    function getValue() {
+        if (isOpClick == false) {
+            oper = this.value;
+            preview.textContent += this.value;
+            isOpClick = true;
+        }
+    }
+}
+
+
+result.addEventListener('click', getResult);
 function getResult() {
-    resOutput = (eval(operand2))
-    if(resOutput==Infinity||isNaN(resOutput)==true){
+    switch (oper) {
+        case '+':
+            inp.value = add(val1, val2); preview.textContent += '=' + inp.value; break;
+
+        case '-':
+            inp.value = sub(val1, val2); preview.textContent += '=' + inp.value; break;
+
+        case '*':
+            inp.value = mul(val1, val2); preview.textContent += '=' + inp.value; break;
+
+        case '/':
+            inp.value = div(val1, val2); preview.textContent += '=' + inp.value; break;
+    }
+}
+
+
+function add(val1, val2) {
+    return parseFloat(val1) + parseFloat(val2);
+}
+function sub(val1, val2) {
+    return parseFloat(val1) - parseFloat(val2);
+}
+function mul(val1, val2) {
+    return parseFloat(val1) * parseFloat(val2);    
+}
+function div(val1, val2) {
+    if(val1==0&&val2==0||val2==0){
         return 'Error'
     }
-    return (resOutput);
+   return val1/val2;
+   //return Math.floor(val1/val2* 100) / 100 ;
 }
-clear.addEventListener('click', clearInp)
-function clearInp() {
+
+
+
+clear.addEventListener('click', clearInp);
+function clearInp(e) {
     inp.value = '';
     preview.textContent = '';
-    operand1 = undefined;
-    operand2 = undefined;
-    operator = undefined;
+    val1 = '';
+    val2 = '';
+    oper = '';
+    isOpClick = false;
+    isDot = false;
 }
+
+
+
