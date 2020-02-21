96191dcc689bfe53651057f637cc5cbb65f00934
Nina Herasymova <predvoditeldvoryanstva@gmail.com>
2020-02-21 15:36:34 +0200

    serial operations fixed

--- src/calculator/script.js
+++ src/calculator/script.js
@@ -10,8 +10,8 @@ let val1 = '';
 let val2 = '';
 let oper = '';
 let isOpClick = false;
-let isDot1=false;
-let isDot2=false;
+let isDot1 = false;
+let isDot2 = false;
 let num;
 
 
@@ -23,21 +23,19 @@ for (let i = 0; i < number.length; i++) {
             val1 += num;
             inp.value = val1;
             preview.textContent += num;
-            return val1;           
+            return val1;
         }
-        if(isOpClick == true){
-            
-        val2 += num;
-        inp.value = val2;
-        preview.textContent += num;
-        return val2;
+        if (isOpClick == true) {
+            val2 += num;
+            inp.value = val2;
+            preview.textContent += num;
+            return val2;
         }
     }
 }
 
 dot.addEventListener('click', addDot);
 function addDot() {
-
     if (isDot1 === false && isOpClick === false) {
         val1 += this.value;
         inp.value += this.value;
@@ -47,16 +45,16 @@ function addDot() {
         val2 += this.value;
         inp.value += this.value;
         preview.textContent += this.value;
-       isDot2 = true;
-    }return;
-} 
-        
-    
+        isDot2 = true;
+    } return;
+}
+
+
 
 for (let i = 0; i < sign.length; i++) {
     sign[i].addEventListener('click', getValue);
     function getValue() {
-        if (isOpClick === false) {            
+        if (isOpClick === false) {
             oper = this.value;
             preview.textContent += this.value;
             isOpClick = true;
@@ -69,19 +67,26 @@ result.addEventListener('click', getResult);
 function getResult() {
     switch (oper) {
         case '+':
-            inp.value = add(val1, val2); preview.textContent += '=' + inp.value; break;
+            inp.value = add(val1, val2); preview.textContent += '=' + inp.value;
+            val1 = inp.value; val2 = ''; isOpClick = false; isDot1 = false;
+            isDot2 = false; console.log(val1, val2); break;
 
         case '-':
-            inp.value = sub(val1, val2); preview.textContent += '=' + inp.value; break;
+            inp.value = sub(val1, val2); preview.textContent += '=' + inp.value;
+            val1 = inp.value; val2 = ''; isOpClick = false; isDot1 = false;
+            isDot2 = false; console.log(val1, val2); break;
 
         case '*':
-            inp.value = mul(val1, val2); preview.textContent += '=' + inp.value; break;
+            inp.value = mul(val1, val2); preview.textContent += '=' + inp.value; val1 = inp.value; val2 = '';
+            isOpClick = false; isDot1 = false;
+            isDot2 = false; console.log(val1, val2); break;
 
         case '/':
-            inp.value = div(val1, val2); preview.textContent += '=' + inp.value; break;
+            inp.value = div(val1, val2); preview.textContent += '=' + inp.value; val1 = inp.value; val2 = '';
+            isOpClick = false; isDot1 = false;
+            isDot2 = false; console.log(val1, val2); break;
     }
 }
-
 
 function add(val1, val2) {
     return parseFloat(val1) + parseFloat(val2);
@@ -90,17 +95,14 @@ function sub(val1, val2) {
     return parseFloat(val1) - parseFloat(val2);
 }
 function mul(val1, val2) {
-    return parseFloat(val1) * parseFloat(val2);    
+    return parseFloat(val1) * parseFloat(val2);
 }
 function div(val1, val2) {
-    if(val1==0&&val2==0||val2==0){
+    if (val1 == 0 && val2 == 0 || val2 == 0) {
         return 'Error'
     }
-   return val1/val2;
-   //return Math.floor(val1/val2* 100) / 100 ;
+    return val1 / val2;
 }
-
-
 
 clear.addEventListener('click', clearInp);
 function clearInp(e) {
