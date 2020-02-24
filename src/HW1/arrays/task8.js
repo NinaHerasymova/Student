69f4a0fc20a1da9927e1052c5b7cec013087b98f
Nina Herasymova <predvoditeldvoryanstva@gmail.com>
2020-02-24 21:47:00 +0200

    del comment

--- src/HW1/arrays/task8.js
+++ src/HW1/arrays/task8.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const viceVersa=(arr)=>{
-    let half = arr.length/2; //if arr.lenght is not even, should be decided, where the odd number should be placed and use Math.floor() or Math.ceil()
+    let half = arr.length/2; 
     let arr1 = arr.splice(0, half);
     return(arr.concat(arr1));
 }
